df9288bba7c0e3c9412e93afdf8703051bcae3a0
kevinhugg <kevinmar704@gmail.com>
2025-08-03 16:14:38 -0300

    Tcc dia 03/08/2025.

--- pages/home.py
+++ pages/home.py
@@ -165,10 +165,10 @@ layout = html.Div([
 def update_static_graphs(theme):
     is_dark = theme == 'dark'
 
-    plot_bg_color = '#295678' if is_dark else 'white'
-    paper_bg_color = '#295678' if is_dark else 'white'
+    plot_bg_color = '#1e40af' if is_dark else 'white'
+    paper_bg_color = '#1e40af' if is_dark else 'white'
     font_color = '#fff' if is_dark else '#000'
-    marker_color = '#f5d100'
+    marker_color = '#FACC15'
     pie_marker_colors = ['#f5d100', '#555' if is_dark else '#d9d9d9']
     map_style = 'carto-darkmatter' if is_dark else 'open-street-map'
 
@@ -241,8 +241,8 @@ def update_static_graphs(theme):
 def att_graph(year_selected, theme):
     is_dark = theme == 'dark'
 
-    plot_bg_color = '#295678' if is_dark else 'white'
-    paper_bg_color = '#295678' if is_dark else 'white'
+    plot_bg_color = '#1e40af' if is_dark else 'white'
+    paper_bg_color = '#1e40af' if is_dark else 'white'
     font_color = '#fff' if is_dark else '#000'
     marker_color = '#f5d100'
 
@@ -282,11 +282,11 @@ def att_flux(n, theme):
 
     if entered > logouted:
         icon_class = 'fas fa-arrow-up'
-        cor = '#40f415'
+        cor = '#10B981'
         flux_text = f'{Agents_loged + entered}'
     else:
         icon_class = 'fas fa-arrow-down'
-        cor = 'red'
+        cor = '#10B981'
         flux_text = f'{Agents_loged - logouted}'
 
     return (
