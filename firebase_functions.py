aaf0187c907447e8bf381813ffa4c854cc411bb5
kevinhugg <kevinmar704@gmail.com>
2025-08-13 19:06:05 -0300

    TCC dia 13/08/25. fazendo o firebase

--- firebase_functions.py
+++ firebase_functions.py
@@ -1,27 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-#inicializa o app uma vez só
+# inicializa o app uma vez só
 if not firebase_admin._apps:
     cred = credentials.Certificate("firebase_config.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-#BUSCAS
 
-#Busca todos as viaturas
+# BUSCAS
+
+# Busca todos as viaturas
 def get_all_vehicles():
     docs = db.collection('veiculos').stream()
     vehicles = [doc.to_dict() for doc in docs]
     return vehicles
 
-#busca viatura por numero
+
+# busca viatura por numero
 def get_vehicle_by_number(numero):
     docs = db.collection('veiculos').where('numero', '==', numero).stream()
     return next((doc.to_dict() for doc in docs), None)
 
-#busca as partes que estao avariadas
+
+# busca as partes que estao avariadas
 def get_all_damage_reports():
     reports = []
     veiculos_ref = db.collection('veiculos').stream()
@@ -41,27 +44,27 @@ def get_all_damage_reports():
             inspecao_data = inspecao.to_dict()
             data_inspecao = inspecao.id
 
-            for parte, info in inspecao_data.items():
-                if isinstance(info, dict) and isinstance(info.get("0"), dict):
-                    parte_info = info.get("0")
-                    descricao = parte_info.get("descricao", "").strip()
-                    uri_foto = parte_info.get("uriFoto", "").strip()
+            for parte, danos in inspecao_data.items():
+                if isinstance(danos, list):
+                    for dano_info in danos:
+                        if isinstance(dano_info, dict):
+                            descricao = dano_info.get("descricao", "").strip()
+                            uri_foto = dano_info.get("uriFoto", "").strip()
 
-                    if descricao or uri_foto:
-                        reports.append({
-                            "viatura": veiculo_data.get('numero', 'Sem número'),
-                            "parte": parte,
-                            "descricao": descricao if descricao else "Sem descrição",
-                            "status": "Aberta",
-                            "data": data_inspecao
-                        })
+                            if descricao or uri_foto:
+                                reports.append({
+                                    "viatura": veiculo_data.get('numero', 'Sem número'),
+                                    "parte": parte,
+                                    "descricao": descricao if descricao else "Sem descrição",
+                                    "status": "Aberta",
+                                    "data": data_inspecao
+                                })
 
     return reports
 
 
 def get_damage_reports_by_vehicle(numero):
     reports = []
-    # Primeiro, encontre o ID do documento do veículo com base no número
     veiculos_ref = db.collection('veiculos').where('numero', '==', numero).limit(1).stream()
     veiculo_doc = next(veiculos_ref, None)
 
@@ -80,21 +83,22 @@ def get_damage_reports_by_vehicle(numero):
             inspecao_data = inspecao.to_dict()
             data_inspecao = inspecao.id
 
-            for parte, info in inspecao_data.items():
-                # A estrutura pode variar, então verificamos se '0' existe e é um dicionário
-                if isinstance(info, dict) and isinstance(info.get("0"), dict):
-                    parte_info = info.get("0")
-                    descricao = parte_info.get("descricao", "").strip()
-                    uri_foto = parte_info.get("uriFoto", "").strip()
+            for parte, danos in inspecao_data.items():
+                if isinstance(danos, list):
+                    for dano_info in danos:
+                        if isinstance(dano_info, dict):
+                            descricao = dano_info.get("descricao", "").strip()
+                            uri_foto = dano_info.get("uriFoto", "").strip()
 
-                    if descricao or uri_foto:
-                        reports.append({
-                            "viatura": veiculo_data.get('numero', 'Sem número'),
-                            "parte": parte,
-                            "descricao": descricao if descricao else "Sem descrição",
-                            "status": "Aberta",  # O status pode precisar de lógica adicional se variar
-                            "data": data_inspecao
-                        })
+                            if descricao or uri_foto:
+                                reports.append({
+                                    "viatura": veiculo_data.get('numero', 'Sem número'),
+                                    "parte": parte,
+                                    "descricao": descricao if descricao else "Sem descrição",
+                                    "status": "Aberta",
+                                    "data": data_inspecao
+                                })
+
     return reports
 
 
@@ -116,18 +120,21 @@ def get_damages_dates():
             inspecao_data = inspecao.to_dict()
             data_inspecao = inspecao.id
 
-            for parte, info in inspecao_data.items():
-                if isinstance(info, dict):
-                    valor_0 = str(info.get("0")).strip() if info.get("0") is not None else ""
-                    descricao = info.get("descricao", "").strip()
-                    uri_foto = info.get("uriFoto", "").strip()
+            for parte, danos in inspecao_data.items():
+                if isinstance(danos, list):
+                    for dano_info in danos:
+                        if isinstance(dano_info, dict):
+                            descricao = dano_info.get("descricao", "").strip()
+                            uri_foto = dano_info.get("uriFoto", "").strip()
 
-                    if valor_0 == "1" or descricao or uri_foto:
-                        dates.append(data_inspecao)
+                            if descricao or uri_foto:
+                                dates.append(data_inspecao)
+                                break
 
     return dates
 
-#busca agentes pela matricula
+
+# busca agentes pela matricula
 def get_agent_by_id(matricula):
     doc_ref = db.collection('agentes').document(matricula)
     doc = doc_ref.get()
@@ -135,23 +142,27 @@ def get_agent_by_id(matricula):
         return doc.to_dict() | {'id': doc.id}
     return None
 
-#buscar ocorrências por viatura
+
+# buscar ocorrências por viatura
 def get_all_agents():
     docs = db.collection('agentes').stream()
     return [doc.to_dict() | {'id': doc.id} for doc in docs]
 
-#agentes sem função
+
+# agentes sem função
 def get_unassigned_agents():
     docs = db.collection('agentes').where('funcao', '==', "").stream()
     return [doc.to_dict() | {'id': doc.id} for doc in docs]
 
-#busca ocorrencias por agente pela subcoleção
+
+# busca ocorrencias por agente pela subcoleção
 def get_ocurrences_by_agent(agent_mat):
     ocorrencias_ref = db.collection('agentes').document(agent_mat).collection('ocorrencias')
     docs = ocorrencias_ref.stream()
     return next((doc.to_dict() | {'matricula': doc.id} for doc in docs))
 
-#busca ocorrencias de todos os agentes na viatura informada
+
+# busca ocorrencias de todos os agentes na viatura informada
 def get_ocurrences_by_vehicles(veiculo_numero):
     ocorrencias = []
 
@@ -175,19 +186,21 @@ def get_ocurrences_by_vehicles(veiculo_numero):
 
     return ocorrencias
 
-#pega agentes com o veiculo
+
+# pega agentes com o veiculo
 def get_agents_by_vehicle(viatura_numero):
     docs = db.collection('agentes').where('viatura', '==', viatura_numero).stream()
     return [doc.to_dict() | {'id': doc.id} for doc in docs]
 
 
-#UPDATES
+# UPDATES
 
-#att agente por id/matricula
+# att agente por id/matricula
 def update_agent(agent_mat, updates: dict):
     db.collection('agentes').document(agent_mat).update(updates)
 
-#remove atribuiçoes do agente
+
+# remove atribuiçoes do agente
 def clear_agent_assignment(agent_mat):
     update_agent(agent_mat, {
         'funcao': '',
